341352017a15836d901cd56d255539fdf98c36ba
Grzegorz Wójcik <w57104@student.wsiz.edu.pl>
2021-10-26 23:18:22 +0200

    first steps at data fetching

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,35 @@
 <template>
-  <Tutorial/>
+  <div>
+    <ul>
+      <li v-for="(recipe, index) in recipes" :key="index">
+        <img :src="recipe.image" :alt="recipe.sourceName">
+        <h3>
+          <nuxt-link :to="`/recipe/${recipe.id}`">
+            {{recipe.title}}
+          </nuxt-link>
+        </h3>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      recipes: []
+    }
+  },
+  async mounted() {
+    const response = await fetch('https://api.spoonacular.com/recipes/random?apiKey=c5d833c8ea6e4742953e7a6380196931', {response: 'json'});
+
+    if(!response.ok) {
+      console.error(`Error ${response.error}`);
+      return;
+    }
+    const recipes = await response.json();
+    this.recipes = recipes["recipes"];
+    console.log(this.recipes);
+  }
+}
 </script>
---
+++ pages/recipe/_id/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <img :src="image" :alt="title">
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      image: ''
+    }
+  },
+  async mounted() {
+    const response = await fetch(`https://api.spoonacular.com/recipes/${this.$route.params.id}/information?apiKey=c5d833c8ea6e4742953e7a6380196931`);
+
+    if(!response.ok) {
+      console.error(`Error ${response.error}`);
+      return;
+    }
+    const recipe = await response.json();
+    this.title = recipe.title;
+    this.image = recipe.image;
+  }
+}
+</script>
